7108cb45728234e2aff7a759464dc2da046e6056
Fred <fcbsd@crowsons.com>
2017-01-04 22:39:13 +0000

    K&R Tutorial Introduction
    A temperature conversion program using some of the K&R approach, but with functions for the conversions and the use of floats rather than integers for those conversions.

---
+++ temperature.c
@@ -0,0 +1,39 @@
+/* 
+ * K&R
+ * Tutorial Introduction
+ * Temperature conversions
+ */
+#include <stdio.h>
+
+#define UPPER	64
+#define LOWER	-64
+#define STEP	16
+
+
+int main(void);
+float celsius(float);
+float fahrenheit(float);
+int header(void);
+
+int main(void) {
+	int i;
+	header();
+	for (i = LOWER; i <= UPPER; i = i + STEP) {
+		printf("%3d\t %6.1f\t %6.1f\n", i, celsius((float) i), fahrenheit((float) i));
+	}
+
+	return 0;
+}
+
+float celsius(float fahr) {
+	return ((5.0 / 9.0) * (fahr - 32));
+}
+
+float fahrenheit(float cels) {
+	return ((9.0 * cels) / 5.0 + 32);
+}
+
+int header(void) {
+	printf("No\tCelcius\tFahrenheit\n");
+	return 0;
+}
