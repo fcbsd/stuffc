b3fa8ea9a44832951dc82a15b4f7a3e82618b6d6
Fred <fcbsd@crowsons.com>
2017-03-01 21:56:13 +0000

    Refactored code
    Create some global variables, and the timediff function - which needs further refactoring to ensure that it can be called recursively - or make it so that it can return multiple values to the calling part of the program.

--- time.c
+++ time.c
@@ -2,52 +2,40 @@
 #include <stdio.h>
 #include <time.h>
 
-<<<<<<< HEAD
-#define TD 60	/*  time delta */
-#define MINS TD /* seconds in a minute */
-#define HRS (MINS * TD) /* seconds in an hour */
+#define MINS 60 /* seconds in a minute */
+#define HRS (MINS * 60) /* seconds in an hour */
 
 int main(void);
 int timediff(int time);
-=======
-int main(void);
->>>>>>> cfde616cf4c93cfad3e2d1f55764dfb6ab52180f
+/* Global variables */
+int hours = 0;
+int mins = 0;
 
 int main(void) {
 	time_t now;
 	time_t then;
 	time_t diff;
-<<<<<<< HEAD
-	int mins;
-	int hours;
-=======
->>>>>>> cfde616cf4c93cfad3e2d1f55764dfb6ab52180f
 
 	now = time(NULL); /* with the null pointer 
 			   * time returns the current time
 			   */
 	then = 1488214716;
 
-	printf("now: %ld\n", now);
-	printf("then: %ld\n", then);
-<<<<<<< HEAD
+	printf("now: %lld\n", now);
+	printf("then: %lld\n", then);
 	printf("In secs h: %d m: %d\n", HRS, MINS);
-=======
->>>>>>> cfde616cf4c93cfad3e2d1f55764dfb6ab52180f
 
 	diff = difftime(now, then);
 
-	printf("diff: %ld\n", diff);
-<<<<<<< HEAD
-	hours, mins = timediff(diff); 
+	printf("diff: %lld\n", diff);
+
+	timediff(diff); 
 	printf("h: %d m: %d\n", hours, mins);
 
 	return 0;
 }
 
 int timediff(int time) {
-	int hours = 0;
-	int mins = 0;
 	int remainder;
 
 	remainder = time % HRS;
@@ -57,11 +45,5 @@ int timediff(int time) {
 	printf("hours: %d\n", hours);
 	printf("mins: %d\n", mins);
 
-	return hours, mins;
-}
-=======
-	 
-
 	return 0;
 }
->>>>>>> cfde616cf4c93cfad3e2d1f55764dfb6ab52180f
