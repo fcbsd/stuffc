6e8a84c891c16c9a078e6f3d5d6f843e0471a710
Fred <fcbsd@crowsons.com>
2017-07-20 17:12:39 +0100

    Creating an Uno Game in C
    Initial idea.

---
+++ uno/uno.c
@@ -0,0 +1,16 @@
+/* Computerised Version of UNO
+ * Copyright (c) 2017 Fred Crowson <fred@crowsons.com>
+ */
+
+#include <stdio.h>
+
+/* Define Colours */
+#define RED ff0000
+#define BLUE 0000ff
+#define GREEN 00ff00
+#define YELLOW ffff00
+
+int main(void);
+int shuffle(); /* shuffle deck funtion */
+int deal(int); /* deal cards to players */
+
