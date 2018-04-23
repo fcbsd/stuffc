f42b2ed53f1e6919fafd6920884f45d78daa4255
Fred <fred@crowsons.com>
2018-04-23 14:25:54 +0100

    Line counting. Added progname to so show what program was called as.

--- lc.c
+++ lc.c
@@ -1,11 +1,14 @@
-/* K&R
+/* Section 1.5.3 K&R
  * Line Counting
  */
 #include <stdio.h>
 
+extern char *__progname; /* program name */
+
 int main(void);
 
 int main(void) {
+	printf("Program called as: %s\n", __progname);
 	int c, nl;
 	nl = 0;
 	while ((c = getchar()) != EOF)
