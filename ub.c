0eb1e7215465f7af6d3ee94bc2b41c30cd2edfde
Fred Crowson <git@crowsons.com>
2018-09-04 12:42:44 +0100

    Change style

--- ub.c
+++ ub.c
@@ -1,10 +1,11 @@
 /* Undefined behaviour
  * https://blog.regehr.org/archives/213
  */
-#include<limits.h>
-#include<stdio.h>
+#include <limits.h>
+#include <stdio.h>
 
-int main (void)
+int 
+main (void)
 {
     printf("%d\n", (INT_MAX+1) < 0);
     return 0;
