db9b4ccd16e769d1d06421014ae5aa0f87bc7d08
Fred Crowson <git@crowsons.com>
2019-02-18 23:49:59 +0000

    Add exercise 1-20

---
+++ tcpl/ex1-20.c
@@ -0,0 +1,10 @@
+/* Exercise 1-20
+ * Write a program detab that replaces tabs
+ * in the input with proper number of blanks
+ * to space to the next tab stop.  Assume a 
+ * fixed set of tab stops, say every n columns.
+ * Should n be a vailable or a symbolic 
+ * parameter?
+ * Page 34
+ */
+
