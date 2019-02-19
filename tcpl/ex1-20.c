27de4e8450497ec441835ff35da1496a3f68d495
Fred <fred@crowsons.com>
2019-02-19 23:25:26 +0000

    intial attempt

--- tcpl/ex1-20.c
+++ tcpl/ex1-20.c
@@ -1,10 +1,47 @@
 /* Exercise 1-20
+ * K&R Page 34
  * Write a program detab that replaces tabs
  * in the input with proper number of blanks
  * to space to the next tab stop.  Assume a 
  * fixed set of tab stops, say every n columns.
  * Should n be a vailable or a symbolic 
  * parameter?
- * Page 34
  */
+#include <stdio.h>
 
+#define TABSTOP 8 /* 8 spaces to a tab - symbolic */
+
+int main(void);
+void detab(char spaces[]);
+
+int
+main (void)
+{
+	int c;
+	int cs = 0; /* count spaces */
+	char line[512];
+
+	while ((c = getchar()) != EOF )
+	{
+		if ( c == '\t') {
+			printf("%s", line);
+		} else {
+			putchar(c);
+		}
+		if ( c == ' ')
+			++cs;
+	}
+
+	return 0;
+}
+
+void
+detab(char spaces[])
+{
+	int i = 0;
+	while ( i < TABSTOP )
+	{
+		spaces[i] = '_';
+		++i;
+	}
+}
