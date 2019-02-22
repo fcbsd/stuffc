253c4199c5116abcc1b8b7fed628a5d4f81a8825
Fred <fred@crowsons.com>
2019-02-22 11:11:08 +0000

    exercise 1-21

---
+++ tcpl/ex1-21.c
@@ -0,0 +1,15 @@
+/* Exercise 1-21
+ * Page 34 K&R
+ *
+ * Write a progam entab that replaces strings of blanks 
+ * by the minimum number of tabs and blanks that
+ * achieve the same spacing.  Use the same tab 
+ * stops as for detab. When either a tab or a 
+ * single blank would suffice to reach a tab stop,
+ * which should be given preference?
+ *
+ * Copyright (c) Fred Crowson <fcbsd@crowsons.com>
+ */
+#include <stdio.h>
+
+
