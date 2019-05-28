61e9d41f5a975129ffa931d773b0f78d688c6575
Fred Crowson <git@crowsons.com>
2019-05-28 14:41:44 +0100

    new exercise descriptions

---
+++ tcpl/ex1-23.c
@@ -0,0 +1,7 @@
+/* Exercise 1-23
+ *
+ * Write a program to remove all comments form a C program.
+ * Don't forget to handle quoted strings and character
+ * constants properly. C comments do not nest.
+ */
+
