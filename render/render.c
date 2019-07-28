f77d8b67c3535078272404466a9881512c352a06
Fred Crowson <git@crowsons.com>
2019-07-29 00:17:32 +0100

    Trying an OpenBSD Version of: Mondern text rendering with Linux: Part 1 from https://mrandri19.github.io/2019/07/18/modern-text-rendering-linux-ep1.html
    OpenBSD has freetype 1.3.1 thus this might have done on a linux machine.

---
+++ render/render.c
@@ -0,0 +1,18 @@
+/*
+ * Modern text rendering with Linux
+ * https://mrandri19.github.io/2019/07/18/modern-text-rendering-linux-ep1.html
+ * 
+ * OpenBSD Version
+ */
+#include <stdio.h>
+#include <freetype2/ft2build.h>
+#include FT_FREETYPE_H
+
+int main(void);
+
+int
+main(void)
+{
+    printf("Hello Render\n");
+    return 0;
+}
