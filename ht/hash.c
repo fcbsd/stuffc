1b91561736e35332e674edc75cff995fbaa58561
Fred Crowson <git@crowsons.com>
2019-08-29 13:07:29 +0100

    Added 5 more insert entries
    This resulted in the program apparently not doing anything so created set the ht_new_sized to 5 instead of 0.
    
    Need to investigate this.

--- ht/hash.c
+++ ht/hash.c
@@ -7,10 +7,16 @@
 
 int main() 
 {
-    ht_hash_table* ht = ht_new_sized(0);
+    ht_hash_table* ht = ht_new_sized(5);
+
+    printf("hashtable created\n");
 
     ht_insert(ht, "cat", "dog in one");
     ht_insert(ht, "one", "information");
+    ht_insert(ht, "two", "truth in 2's");
+    ht_insert(ht, "4", "this is a number?");
+
+    printf("Inserted data\n");
 
     printf("%s \n", ht_search(ht, "cat"));
 
